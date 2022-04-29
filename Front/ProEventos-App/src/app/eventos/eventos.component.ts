82bac0c2112d06275dc26f5a1846d5dc8473279a
lenfc <len.fc@hotmail.com>
2022-04-29 14:27:59 -0300

    Angular

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any;
+  widthImg = 150;
+  marginImg = 2;
+  showImg = true;
+  private _filter: string = '';
+  public eventosFiltrados: any;
 
   constructor(private http: HttpClient) { }
 
@@ -18,9 +23,27 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     )
+  }
+
+  public get filter() {
+    return this._filter;
+  }
+
+  public set filter(value: string) {
+    this._filter = value;
+    this.eventosFiltrados = this.filter ? this.filtrarEventos(this.filter) : this.eventos;
+  }
+
+  filtrarEventos(filterBy: string) {
+    filterBy = filterBy.toLowerCase();
+    return this.eventos.filter( (evento: { tema: string; local: string; }) => evento.tema.toLowerCase().indexOf(filterBy) !== -1 ||
+    evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1)
   }
 
 }
